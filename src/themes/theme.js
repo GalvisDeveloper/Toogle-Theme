1e5b5d664b9eed582d3d3563216c340e41f6d2bd
GalvisDeveloper <galvisdeveloper@gmail.com>
2021-03-12 22:46:55 -0500

    Global styles and custom theme

---
+++ src/themes/theme.js
@@ -0,0 +1,21 @@
+
+
+export const darkTheme = {
+    body: '',
+    text: '',
+    toogleBorder: '',
+    gradient: 'linear-gradient()',
+    buttonColor: '',
+    buttonTextColor: '',
+    toogleSwitch: '',
+}
+
+export const lightTheme = {
+    body: '',
+    text: '',
+    toogleBorder: '',
+    gradient: 'linear-gradient()',
+    buttonColor: '',
+    buttonTextColor: '',
+    toogleSwitch: '',
+}
